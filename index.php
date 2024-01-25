5cbc187bc2c1e42c395094d1e65413d8df04508f
Anonym0usLemon <76603698+Anonym0usLemon@users.noreply.github.com>
2024-01-25 12:32:49 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -3,26 +3,37 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="./styles.css">
+  <link rel="stylesheet" href="./styles/styles.css">
 </head>
 
 <body>
   <div class="modal">
-    <section>
-
-    </section>
-
-    <section>
-
-    </section>
+    <a class="logo-link" href="http://acilab.com">
+      <img src="./images/logo.jpg" alt="">
+    </a>
 
     <form class="email-form" method="post" action="post.php">
-      <label for="email">Enter your email to recieve your 10 free backgrounds.</label>
-      <input id="email" type="text" name="email" placeholder="Enter your Email" />
-      <button type="submit">Submit</button>
+      <label for="email">Step up your photography with our stunning backgrounds! <br><br> <span class="tiny">Subscribe to our newsletter to receive a free sampler pack.</span> </label>
+      <div class="container">
+        <input id="email" type="email" name="email" placeholder="Your email address" />
+        <button type="submit">Submit</button>
+      </div>
     </form>
+
+    <section class="promo">
+      <a href="https://backgroundtown.com/">
+        <img src="./images/SPAC-Deals-01.jpg" alt="">
+      </a>
+    </section>
+
+    <section class="promo">
+      <a href="https://backgroundtown.com/">
+        <img src="./images/SPAC-Deals-02.jpg" alt="">
+      </a>
+    </section>
+
     <!-- <a href="./bg.zip">download here</a> -->
   </div>
 </body>
--- post.php
+++ post.php
@@ -1,39 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thank you!</title>
+  <link rel="stylesheet" href="./styles/post.css">
+</head>
+<body>
 <?
 // Path to lib file with all the helper functions 
 // \\172.16.16.156\e$\lib
-require_once('/172.16.16.156/e$/lib/common.php');
+define('LIB', "E:\\lib\\");
+require_once(LIB . "common.php");
 
-// Validate Email. 
+// // Validate Email. 
 if (is_valid_email($_POST['email'])) {
-  echo "Email is valid";
   $email = $_POST['email'];
+
+  // Store the email in emails.php if it's valid. 
+  // Load existing emails from file if any
+  $existingEmails = file_get_contents('emails.php');
+  // Append the new email with a new line
+  $newContent = $existingEmails . PHP_EOL . $email;
+  // Write the new content back to the file
+  file_put_contents('emails.php', $newContent);
+
+  // Store the email contents here
+  $body = 'here\'s the link to your download <br/> <a href="https://acilab.com/SPAC_2024/ACI_Background_Sampler.zip">background sampler pack</a>';
+  html_email($email, 'info@acilab.com', 'ACI Background Sampler Pack', $body);
+
+  echo '<div class="success-message">
+          <h1>Thank You</h1>
+          <p>We\'ve emailed you a link to download your backgrounds. Be sure to check your spam folder!</p>
+        </div>'; 
 } else { // redirect to index.php and use flashdata to display an error message if not valid. 
   echo "Invalid Email";
   //header('Location: index.php?error=invalid_email');
-  exit; 
+  exit;
 }
 
 
-//Store the email in emails.php if it's valid. 
-
-// Read the existing content of the file, unserialize it, and convert it to an array
-$existingEmails = file_exists('emails.php') ? unserialize(file_get_contents('emails.php')) : [];
-
-// Add the new email to the array
-$newEmail = $email;
-$existingEmails[] = $newEmail;
-
-// Serialize the array and save it back to the file
-file_put_contents('emails.php', serialize($existingEmails));
-
-
-// Store the email contents here
-$body = 'here\'s the link to your download <br/> <a href="./bg.zip">background sampler pack</a>';
-
-
-html_email($email, 'info@acilab.com', ' Heres you free stuff!', $body );
-
-
-// redirect to a success page.
-
 ?>
+</body>
+</html>
